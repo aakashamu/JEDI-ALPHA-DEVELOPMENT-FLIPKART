f08ad1bc79d0365f580aba8020fa1b64612aae4a
ananya-s26 <ananya.sangani26@gmail.com>
2026-01-23 12:31:47 +0530

    Added initial directory structure

---
+++ JEDI_FLIPFIT_JAVA_POS/src/com/flipfit/business/NotificationService.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+package com.flipflit.business;
+
+/**
+ * 
+ */
+public class NotificationService {
+
+}
--- java-project/Demo.java
+++
@@ -1,6 +0,0 @@
-class Demo{
-	public static void main(String[] arr)
-	{
-		System.out.println("This is my first application.");
-	}
-}
