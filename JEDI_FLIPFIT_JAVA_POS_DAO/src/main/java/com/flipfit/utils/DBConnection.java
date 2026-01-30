c1b5ad7c5253258345146a4c48e5dfaf297ec468
Chandak-Keshav <keshavchandak2408@gmail.com>
2026-01-30 09:22:01 +0530

    Added more exceptions and handled some logical issues with the code

--- JEDI_FLIPFIT_JAVA_POS_DAO/src/main/java/com/flipfit/utils/DBConnection.java
+++ JEDI_FLIPFIT_JAVA_POS_DAO/src/main/java/com/flipfit/utils/DBConnection.java
@@ -16,7 +16,7 @@ public class DBConnection {
     // Database credentials
     private static final String URL = "jdbc:mysql://localhost:3306/FlipFitDB";
     private static final String USER = "root";
-    private static final String PASSWORD = "Niu2Xei2!@#%";
+    private static final String PASSWORD = "";
 
     /**
      * Get Connection.
